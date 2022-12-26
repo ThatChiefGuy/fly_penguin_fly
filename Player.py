96623899e96d1e13de3a4f98d277c7ce6a39fd0b
ThatChiefGuy <peroperje@gmail.com>
2022-12-26 13:47:10 +0100

    added penguin.png

--- Player.py
+++ Player.py
@@ -1,2 +1,8 @@
 import pygame
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.image.load("player.png").convert_alpha()
+        self.image.set_colorkey((246, 246, 246))
