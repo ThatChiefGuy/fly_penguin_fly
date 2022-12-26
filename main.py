b760fc61392f8a39b704b58651fe20704ff756b5
ThatChiefGuy <peroperje@gmail.com>
2022-12-26 17:41:58 +0100

    added scrolling backgound

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
 import Player
+import math
 
 
 class Game:
     def __init__(self, window_size_x, window_size_y, title, fps):
-        self.window = pygame.display.set_mode((window_size_x, window_size_y))
+        pygame.init()
+        self.window_width = window_size_x
+        self.window_height = window_size_y
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
         pygame.display.set_caption(title)
         self.Fps = fps
+        back_ground = pygame.image.load("backgound.jpg").convert_alpha()
+        self.back_ground = pygame.transform.scale(back_ground, (700, 350))
+        self.back_ground_height = back_ground.get_height()
+        self.tiles = math.ceil(self.window_height / self.back_ground_height) + 1
+        self.scroll = 0
 
     def draw(self, draw_player):
         self.window.fill((43, 208, 237))
+
+        for i in range(0, self.tiles):
+            self.window.blit(self.back_ground, (0, i * self.back_ground_height + self.scroll))
+
         draw_player.player_group.draw(self.window)
         pygame.display.update()
 
@@ -21,6 +34,10 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+            self.scroll -= 2
+            if abs(self.scroll) > self.back_ground_height:
+                self.scroll = 0
 
             self.draw(draw_player)
         pygame.quit()
